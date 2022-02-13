9e66b2ab283859df48115e9030595533135ad293
AlexMAK1 <3149238@gmail.com>
2022-02-13 07:58:04 -0400

    Версия 2. Извиняюсь, не сразу разобрался где и как смотреть замечания.

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -2,37 +2,30 @@ import java.util.HashMap;
 
 public class StepTracker {
     HashMap<Integer, MonthData> monthToData = new HashMap<>();
+
     int goal = 10000;
 
-
     public StepTracker() {
-
-
         for (int i = 0; i < 12; i++) {
             monthToData.put(i, new MonthData());
         }
-
     }
 
     void saveDayData(int nameMonth, int steps, int numberDay) {
-
         MonthData result = monthToData.get(nameMonth - 1);
         result.add(numberDay, steps);
-
     }
 
     void printDataMonth(int nameMonth) {
         MonthData result = monthToData.get(nameMonth - 1);
         result.print();
         System.out.println(" ");
-        System.out.println("Общее количество шагов за месяц:" + result.sum());
+        System.out.println("Общее количество шагов за месяц:" + result.findsum());
         System.out.println("Максимальное пройденное количество шагов в месяце:" + result.findmax());
-        System.out.println("Среднее количество шагов за месяц:" + result.average());
-        System.out.println("Пройденная дистанция (в км) за месяц:" + result.distance());
-        System.out.println("Общее количество сожжённых килокалорий:" + result.kKal());
-        System.out.println("Лучшая серия: максимальное количество подряд идущих дней, в течение которых количество шагов за день было равно или выше целевого:" + result.bestseries(goal));
-
-
+        System.out.println("Среднее количество шагов за месяц:" + result.findaverage());
+        System.out.println("Пройденная дистанция (в км) за месяц:" + result.finddistance());
+        System.out.println("Общее количество сожжённых килокалорий:" + result.findkkal());
+        System.out.println("Лучшая серия: максимальное количество подряд идущих дней, в течение которых количество шагов за день было равно или выше целевого:" + result.findbestseller(goal));
     }
 
     void changeGoal(int target) {
@@ -43,8 +36,6 @@ public class StepTracker {
             System.out.println("Количество шагов должно быть неотрицательным");
         }
     }
-
-
 }
 
 
